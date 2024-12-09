47fa40883b8cd8a83fae3f948dead3353ba7b7e3
WoominWooing <changwoomin426@gmail.com>
2024-12-10 02:14:53 +0900

    [Feat] 프로젝트 생성

---
+++ Indoor/Assets/02_Scripts/SetNavigationTarget.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using TMPro;
+
+public class SetNavigationTarget : MonoBehaviour
+{
+    // [SerializeField] private Camera topDownCamera;
+    // [SerializeField] private GameObject navTargetObject;
+
+    [SerializeField] private TMP_Dropdown navigationTargetDropDown;
+    [SerializeField] private List<Target> navigationTargetObjects = new List<Target>();
+
+    private NavMeshPath path;
+    private LineRenderer line;
+    private Vector3 targetPosition = Vector3.zero;
+
+    private bool lineToggle = false;
+
+    private void Start()
+    {
+        path = new NavMeshPath();
+        line = transform.GetComponent<LineRenderer>();
+        line.enabled = lineToggle;
+    }
+
+    private void Update()
+    {
+        if (lineToggle && targetPosition != Vector3.zero)
+        {
+            NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
+            line.positionCount = path.corners.Length;
+            line.SetPositions(path.corners);
+        }
+    }
+
+    public void SetCurrentNavigationTarget(int selecedValue)
+    {
+        targetPosition = Vector3.zero;
+        string selectedText = navigationTargetDropDown.options[selecedValue].text;
+        Target currentTarget = navigationTargetObjects.Find(x => x.Name.Equals(selectedText));
+
+        if (currentTarget != null)
+        {
+            targetPosition = currentTarget.PositionObject.transform.position;
+        }
+    }
+
+    public void ToggleVisibility()
+    {
+        lineToggle = !lineToggle;
+        line.enabled = lineToggle;
+    }
+}
